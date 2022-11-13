db215864eaeef6275466f8d951e99b28ea35f9bc
Sergei Podkopaev <hardaoko@gmail.com>
2022-11-13 12:24:05 +0300

    Верстка билета

--- src/pages/AviaInfo/AviaInfo.tsx
+++ src/pages/AviaInfo/AviaInfo.tsx
@@ -4,12 +4,24 @@ import { useMySelector } from "../../utils/types";
 import { ticketInfo } from "../../utils/constants";
 import LuggageLarge from "../../images/LuggageLarge.svg";
 import LuggageSmall from "../../images/LuggageSmall.svg";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AviaInfo = () => {
   const { from, to, departure, arrival } = useMySelector((store) => store);
+  const [numFlight, setNumFlight] = useState(0);
+  const navigate = useNavigate();
   const wayTime = "1 ч 55 минут";
   const airportFrom = "SVO";
   const airportTo = "ROV";
+
+  useEffect(() => {
+    (from === "" || to === "") && navigate("/avia");
+  }, []);
+
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    setNumFlight(Number.parseInt(e.currentTarget.value));
+  };
   return (
     <div className="avia_info_container">
       <div className="ticket_container">
@@ -21,7 +33,11 @@ export const AviaInfo = () => {
         <section className="center_side_ticket">
           <div className="center_up_box">
             <div className="departure_box">
-              <p className="text_time">{ticketInfo.departure}</p>
+              <p className="text_time">
+                {ticketInfo[numFlight].departure.charAt(0) === "0"
+                  ? ticketInfo[numFlight].departure.substring(1)
+                  : ticketInfo[numFlight].departure}
+              </p>
               <p className="text_city_date">
                 {from}
                 <br />
@@ -35,7 +51,7 @@ export const AviaInfo = () => {
               <p className="text_way_time">Время в пути {wayTime}</p>
             </div>
             <div className="destination_box">
-              <p className="text_time">{ticketInfo.arrival}</p>
+              <p className="text_time">{ticketInfo[numFlight].arrival}</p>
               <p className="text_city_date">
                 {to}
                 <br />
@@ -47,10 +63,28 @@ export const AviaInfo = () => {
               <img src={LuggageLarge} />
             </div>
           </div>
-          <div className="center_down_box"></div>
+          <div className="center_down_box">
+            {ticketInfo.map((ticket, index) => {
+              return (
+                <div className="form_radio_btn" key={index}>
+                  <input
+                    id={`radio-${index}`}
+                    type="radio"
+                    name="radio"
+                    value={index}
+                    onClick={handleClick}
+                    defaultChecked={index === 0}
+                  />
+                  <label htmlFor={`radio-${index}`}>
+                    {ticket.departure} - <span>{ticket.arrival}</span>
+                  </label>
+                </div>
+              );
+            })}
+          </div>
         </section>
         <section className="right_side_ticket">
-          <div className="price_box">{ticketInfo.price} ₽</div>
+          <div className="price_box">{ticketInfo[numFlight].price} ₽</div>
         </section>
       </div>
     </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,17 @@
-export const ticketInfo = {
-  departure: "9:20",
-  arrival: "11:05",
-  price: (Math.random() * 2000 + 6000).toFixed(0),
-};
+export const ticketInfo = [
+  {
+    departure: "09:20",
+    arrival: "11:05",
+    price: (Math.random() * 2000 + 6000).toFixed(0),
+  },
+  {
+    departure: "10:20",
+    arrival: "12:05",
+    price: (Math.random() * 2000 + 6000).toFixed(0),
+  },
+  {
+    departure: "11:20",
+    arrival: "13:05",
+    price: (Math.random() * 2000 + 6000).toFixed(0),
+  },
+];
